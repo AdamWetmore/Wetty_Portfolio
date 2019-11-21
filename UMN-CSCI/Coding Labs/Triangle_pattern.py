e3b0d7d78f4ae8e079e84e385a46bfa15897cf13
Adam Wetmore <adam.r.wetmore@gmail.com>
2019-11-21 14:19:27 -0600

    Updated

--- UMN-CSCI/Coding Labs/Triangle_pattern.py
+++ UMN-CSCI/Coding Labs/Triangle_pattern.py
@@ -1,8 +1,16 @@
 import turtle
 
-def draw_triangles(number):
+def draw_triangle(t):
+    for i in range(3):
+        t.forward(50)
+        t.left(120)
+
+def draw_triangles(number,t):
     count = 0
     while number > count:
-        draw_triangle()
-        turtle.rt(360 / number)
+        draw_triangle(t)
+        t.rt(360 / number)
         count += 1
+
+
+
